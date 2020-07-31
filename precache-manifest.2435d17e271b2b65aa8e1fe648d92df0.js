9382f15ffc3727faf1d895cd83fc512afe88eb71
Raghavendra0008 <graghavendra413@gmail.com>
2020-07-31 18:42:12 +0530

    Updates

---
+++ precache-manifest.2435d17e271b2b65aa8e1fe648d92df0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d34d8d47ec54e5eeee59cb1817befbab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42f1071bf5a5d779f9e6",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5666bba75977ad74e523",
+    "url": "/static/css/main.f44cb3ab.chunk.css"
+  },
+  {
+    "revision": "42f1071bf5a5d779f9e6",
+    "url": "/static/js/2.3b3ed4d9.chunk.js"
+  },
+  {
+    "revision": "48569a0863e86e8b00cf55db963d6025",
+    "url": "/static/js/2.3b3ed4d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5666bba75977ad74e523",
+    "url": "/static/js/main.4e0b4bd2.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "d8e1a8faddf8eac772fb7848726f69fa",
+    "url": "/static/media/himg.d8e1a8fa.jpg"
+  },
+  {
+    "revision": "c7de6e19e7672908bd13c33a0898e8b4",
+    "url": "/static/media/img1.c7de6e19.jpg"
+  }
+]);
